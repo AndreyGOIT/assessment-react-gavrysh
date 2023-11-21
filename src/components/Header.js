44f0a0c8328b67578df7a985fdc65beec3cad812
AndreyGOIT <ae.borga@gmail.com>
2023-11-21 15:52:44 +0200

    router & redux

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Мое React-приложение</h1>
+      {/* Добавьте другие элементы заголовка здесь */}
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import { useSelector } from "react-redux";
+
+const Home = () => {
+  //   const data = useSelector((state: any) => state.data); // Подставьте нужный селектор
+
+  return (
+    <div>
+      <h2>Домашняя страница</h2>
+      {/* Используйте данные из бэкенда здесь */}
+    </div>
+  );
+};
+
+export default Home;
