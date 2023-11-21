99dee27d323a521e6862245d5555d67d3ba79449
AndreyGOIT <ae.borga@gmail.com>
2023-11-21 16:43:49 +0200

    basics in todo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,20 @@
 import React from "react";
 // import { useSelector } from "react-redux";
+import { Layout } from "./Layout/Layout";
+import { AppBar } from "./AppBar/AppBar";
+import { TaskForm } from "./TaskForm/TaskForm";
+import { TaskList } from "./TaskList/TaskList";
 
 const Home = () => {
   //   const data = useSelector((state: any) => state.data); // Подставьте нужный селектор
 
   return (
-    <div>
-      <h2>Домашняя страница</h2>
+    <Layout>
+      <AppBar />
+      <TaskForm />
+      <TaskList />
       {/* Используйте данные из бэкенда здесь */}
-    </div>
+    </Layout>
   );
 };
 
