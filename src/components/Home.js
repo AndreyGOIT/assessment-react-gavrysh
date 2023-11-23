426ea358e6f0f073ab0ac060ed6254e8641c1bc4
AndreyGOIT <ae.borga@gmail.com>
2023-11-23 15:47:11 +0200

    redux ok

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,10 @@
 import React from "react";
-// import { useSelector } from "react-redux";
 import { Layout } from "./Layout/Layout";
 import { AppBar } from "./AppBar/AppBar";
 import { TaskForm } from "./TaskForm/TaskForm";
 import { TaskList } from "./TaskList/TaskList";
 
 const Home = () => {
-  //   const data = useSelector((state: any) => state.data); // Подставьте нужный селектор
-
   return (
     <Layout>
       <AppBar />
